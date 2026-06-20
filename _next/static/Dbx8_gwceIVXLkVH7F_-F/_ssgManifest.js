757f599501de72e8fde38f3ac8de3b0f995f9782
MzdrowY <mzdrowy@users.noreply.github.com>
2026-06-20 23:37:57 +0200

    deploy: neon CTA on program listing cards

---
+++ _next/static/Dbx8_gwceIVXLkVH7F_-F/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fksiazki","\u002F[locale]\u002Fksiazki\u002F[slug]","\u002F[locale]\u002Fprogramy","\u002F[locale]\u002Fprogramy\u002F[slug]","\u002F[locale]\u002Fsugestie"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
